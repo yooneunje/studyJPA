fa17d407c08c77c27d2232682681cc344feb934c
yooneunje <87642401+yooneunje@users.noreply.github.com>
2022-06-20 16:10:42 +0900

    add

--- ex1-hello-jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -7,6 +7,7 @@ public class JpaMain {
 
     public static void main(String[] args) {
         //데이터 베이스당 하나만 생성
+        //persistence.xml 의 hello 라는 이름을 가진 설정 정보를 참고하여 만들어진다.
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
         //요청이 올때 마다 사용 후 close로 버림. 쓰레드간에 공유 절대 금지!
